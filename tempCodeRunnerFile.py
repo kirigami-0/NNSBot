6152adfb33e7e13c194159348d9572589df00b4f
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-19 23:47:44 +0900

    TOKEN導入

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,9 +1,4 @@
-import discord
-from discord.ext import commands
-import os
-from dotenv import load_dotenv
-load_dotenv()
-intents = discord.Intents.default()
+
 bot = commands.Bot(command_prefix='n.', intents=intents)
 
 @bot.event
