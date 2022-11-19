cc7690f75cb606ad30003d8a701d83aa08144dd4
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-19 23:26:44 +0900

    テストコミット

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 print("始まり")
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+
+
+bot = commands.Bot(command_prefix='n.', intents=intents)
+
+@bot.event
+async def on_ready():
+    print("ボット起動")
+
+bot.run('token')
