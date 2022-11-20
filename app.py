df7e553e5053248df50cb6868babd9208e68c9a2
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 17:30:10 +0900

    bot起動?

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
 load_dotenv()
 
 from flask import Flask
 app = Flask(__name__)
+bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all())
 
 @app.route('/')
 def hello_world():
-    print(os.getenv("TOKEN"))
-    print("123456789")
+    
+    # print(os.getenv("TOKEN"))
+    # print("123456789")
+    bot.run(os.getenv("TOKEN"))
     return 'Hello, World!'
