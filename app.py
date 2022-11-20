f10d8ccbf32f90fc2baedac32bbd3c118ffbf45f
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 17:09:07 +0900

    envファイル読み込み

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
+import discord
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    print(os.environ.get("TOKEN"))
     print("123456789")
     return 'Hello, World!'
