d297bd41c4b1442fe1db284a2d55fed662c0979a
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 22:22:33 +0900

    test

--- app.py
+++ app.py
@@ -3,24 +3,46 @@ import os
 import json
 from dotenv import load_dotenv
 from discord.ext import commands
-
+from flask import Flask, render_template, request, session
+from oauth import Oauth
 load_dotenv()
 json_load = json.load(open('cog.json', 'r'))
 
-from flask import Flask
 app = Flask(__name__)
-bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all())
-
-print("123456789")
-@bot.event
-async def on_ready():
-    print("ボット起動")
-    for cog_name in json_load["cog"]:
-        print(cog_name)
-        await bot.load_extension(f"Cog.{cog_name}")
-        return "123456789"
+app.config["SECRET_KEY"] = "test"
 
 @app.route('/')
-def hello_world():
-    bot.run(os.getenv("TOKEN"))
-    return 'Hello, World!'
+def home():
+    return render_template("index.html", discord_url= Oauth.discord_login_url)
+
+@app.route('/login')
+def login ():
+    code = request.args.get("code")
+    access_token = Oauth.get_access_token(code)
+    session["token"] = access_token
+
+    user = Oauth.get_user_json(access_token)
+    user_name, user_id = user.get("username"), user.get("discriminator")
+    return f"成功 {user_name}#{access_token}"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+# bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all())
+
+# print("123456789")
+# @bot.event
+# async def on_ready():
+#     print("ボット起動")
+#     for cog_name in json_load["cog"]:
+#         print(cog_name)
+#         await bot.load_extension(f"Cog.{cog_name}")
+#         return "123456789"
+
+# @app.route('/')
+# def hello_world():
+#     bot.run(os.getenv("TOKEN"))
+#     return 'Hello, World!'
