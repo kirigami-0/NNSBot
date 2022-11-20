71dd4af87649d265051a8b2517049b9983d4cbc6
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 16:11:55 +0900

    flask

--- main.py
+++ main.py
@@ -2,12 +2,24 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix='n.', intents=intents)
+
 
 @bot.event
 async def on_ready():
     print("ボット起動")
 
-bot.run(os.environ["TOKEN"])
+try:
+    bot.run(os.environ["TOKEN"])
+except Exception as e:
+    print(e.status_code)
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def start():
+    return "123"
