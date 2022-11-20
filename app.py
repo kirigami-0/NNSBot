7f78a936d9f85611598a4749d6242da8e2e203ce
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 18:07:02 +0900

    bot起動????

--- app.py
+++ app.py
@@ -11,15 +11,17 @@ from flask import Flask
 app = Flask(__name__)
 bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all())
 
-@bot.event
-async def on_ready():
-    print("ボット起動")
-    for cog_name in json_load["cog"]:
-        print(cog_name)
-        await bot.load_extension(f"Cog.{cog_name}")
+def start():
+    print("123456789")
+    @bot.event
+    async def on_ready():
+        print("ボット起動")
+        for cog_name in json_load["cog"]:
+            print(cog_name)
+            await bot.load_extension(f"Cog.{cog_name}")
 
 @app.route('/')
 def hello_world():
-    
+    start()
     bot.run(os.getenv("TOKEN"))
     return 'Hello, World!'
