c97e061c5fbcdc65f8f6fcf14e7d18406a9d0730
github <71970893+kirigami-0@users.noreply.github.com>
2022-11-20 19:09:56 +0900

    bot起動???????

--- app.py
+++ app.py
@@ -8,21 +8,19 @@ load_dotenv()
 json_load = json.load(open('cog.json', 'r'))
 
 from flask import Flask
-# app = Flask(__name__)
-bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all()), Flask(__name__)
+app = Flask(__name__)
+bot = commands.Bot(command_prefix='n.', intents=discord.Intents.all())
 
-def start():
-    print("123456789")
-    @bot.event
-    async def on_ready():
-        print("ボット起動")
-        for cog_name in json_load["cog"]:
-            print(cog_name)
-            await bot.load_extension(f"Cog.{cog_name}")
-    return "123456789"
+print("123456789")
+@bot.event
+async def on_ready():
+    print("ボット起動")
+    for cog_name in json_load["cog"]:
+        print(cog_name)
+        await bot.load_extension(f"Cog.{cog_name}")
+        return "123456789"
 
-@bot.route('/')
+@app.route('/')
 def hello_world():
-    start()
     bot.run(os.getenv("TOKEN"))
     return 'Hello, World!'
